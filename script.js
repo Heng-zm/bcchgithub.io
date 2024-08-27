1c2e1c8d521472a591303c51244dd7b434461185
ByBor <107470944+Heng-zm@users.noreply.github.com>
2024-08-27 19:44:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const handleChat = () => {
 
   setTimeout(() => {
     // Display "Thinking..." message while waiting for the response
-    const incomingChatLi = createChatLi("Thinking...", "incoming");
+    const incomingChatLi = createChatLi("Ai កំពុងគិត ......", "incoming");
     chatbox.appendChild(incomingChatLi);
     chatbox.scrollTo(0, chatbox.scrollHeight);
     generateResponse(incomingChatLi);
